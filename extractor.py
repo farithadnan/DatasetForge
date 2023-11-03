7c9a3393c07f201f3568a84d876f8144afd661ab
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2023-11-03 11:48:29 +0800

    Add logging and configuration support to GSpreadClient and DataHandler classes

--- extractor.py
+++ extractor.py
@@ -1,38 +1,47 @@
 import gspread
 import tiktoken
+import logging
+from omegaconf import DictConfig
 from typing import List, Dict
 
-# Class that handles the authentication with Google Sheets
 class GSpreadClient:
-    # Constructor
-    def __init__(self, config_path: str):
-        self.config_path = config_path
+    def __init__(self):
+        pass
 
-    # Method to authenticate with Google Sheets
-    def Authenticate(self):
+    def Authenticate(self, config_path: str):
+        '''
+        Method to authenticate with Google Sheets
+
+        Returns:
+            The authenticated Google Sheets client
+        '''
         try:
-            # Authenticate with Google Sheets
-            gc = gspread.service_account(filename=self.config_path)
-
-            # Return the authenticated client
+            gc = gspread.service_account(filename=config_path)
             return gc
-        
-        # Raise exception if any error occurs
+        except FileNotFoundError as e:
+            raise RuntimeError(f"Failed to authenticate with Google Sheets due to missing credentials file: {e}")  
         except Exception as e:
-            raise RuntimeError(f"Failed to authenticate with Google Sheets: {e}")
+            raise RuntimeError(f"Failed to authenticate with Google Sheets due to unexpected error: {e}")
 
 
-# Class to extract data from Google Sheets
 class DataHandler:
-    # Constructor
-    def __init__(self, gspread_client: gspread.client, google_sheet_url: str):
+    def __init__(self, cfg: DictConfig, gspread_client: gspread.client, google_sheet_url: str):
+        self.openai_model = cfg.openAI.model
         self.gspread_client = gspread_client
         self.google_sheet_url = google_sheet_url
 
-    # Method to extract data from Google Sheets
     def extract_data(self, sheet_index: int) -> List[Dict[str, str]]:
+        '''
+        Method to extract data from Google Sheets
+
+        Args:
+            sheet_index: The index of the sheet to extract data from
+
+        Returns:
+            The extracted data
+        '''
+        logger = logging.getLogger("DataHandler")
         try:
-            # Extract data from Google Sheets
             sh = self.gspread_client.open_by_url(self.google_sheet_url)
             worksheet = sh.get_worksheet(sheet_index)
             rows = worksheet.get_all_values()
@@ -59,60 +68,74 @@ class DataHandler:
                     processed_data.extend(processed_data_item)
                 
             # Print the output
-            print(f"\nTotal word count: {total_word_counts}")
-            print(f"Estimated tokens: {total_token_counts}")
-            print(f"Estimated cost of fine-tuning: ${total_estimated_costs}\n")
+            logger.info(f"Total word count: {total_word_counts}")
+            logger.info(f"Estimated tokens: {total_token_counts}")
+            logger.info(f"Estimated cost of fine-tuning: ${total_estimated_costs}\n")
 
             # Return the processed data
             return processed_data
         
-        # Raise exception if any error occurs
+        except (AttributeError, KeyError) as e:
+            print(f"An AttributeError or KeyError occurred: {e}")
         except Exception as e:
             raise RuntimeError(f"Failed to extract data from Google Sheets: {e}")
 
 
-    # Method to process the data
     def filter_data(self, prompt: str, completion: str) -> List[Dict[str, str]]:
         filtered_data = []
+        try:
+            # Remove leading & trailing whitepace in the left & the right
+            prompt = prompt.lstrip().rstrip()
+            completion = completion.lstrip().rstrip()
 
-        # Remove leading & trailing whitepace in the left & the right
-        prompt = prompt.lstrip().rstrip()
-        completion = completion.lstrip().rstrip()
+            # Check and add "\n\n###\n\n" at the end of prompt
+            if not prompt.endswith("\n\n###\n\n"):
+                prompt += " \n\n###\n\n"
+            
+            # Check and add "." at the end of completion
+            if not completion.endswith("."):
+                completion += "."
 
-        # Check and add "\n\n###\n\n" at the end of prompt
-        if not prompt.endswith("\n\n###\n\n"):
-            prompt += " \n\n###\n\n"
+            # Check and add "END" at the end of completion
+            if not completion.endswith("END"):
+                completion += " END"
+            
+            ## Check and add " " at the front of completion
+            if not completion.startswith(" "):
+                completion = " " + completion
+            
+            # Check tokens validity
+            total_word_count, total_token_count, estimated_cost = self.check_tokens(prompt, completion)
+
+            # Append the processed data
+            filtered_data.append({"prompt": prompt, "completion": completion})
+
+            # Return the processed data
+            return filtered_data, total_word_count, total_token_count, estimated_cost
         
-        # Check and add "." at the end of completion
-        if not completion.endswith("."):
-            completion += "."
-
-        # Check and add "END" at the end of completion
-        if not completion.endswith("END"):
-            completion += " END"
-        
-        ## Check and add " " at the front of completion
-        if not completion.startswith(" "):
-            completion = " " + completion
-        
-        # Check tokens validity
-        total_word_count, total_token_count, estimated_cost = self.check_tokens(prompt, completion)
-
-        # Append the processed data
-        filtered_data.append({"prompt": prompt, "completion": completion})
-
-        # Return the processed data
-        return filtered_data, total_word_count, total_token_count, estimated_cost
+        except (AttributeError, KeyError) as e:
+            print(f"An AttributeError or KeyError occurred: {e}")
+        except Exception as e:
+            raise RuntimeError(f"Failed to extract data from Google Sheets: {e}")
 
 
-    # Method to check tokens
     def check_tokens(self, prompt: str, completion: str):
+        '''
+        Method to check the number of tokens
+
+        Args:
+            prompt: The prompt text
+            completion: The completion text
+        
+        Returns:
+            The total number of words, the total number of tokens, and the estimated cost
+        '''
         try:
             # Concatenate prompt and completion
             combined_text = prompt + " " + completion
 
             # Create a GPT-3 encoder instance
-            encoder = tiktoken.get_encoding("r50k_base")
+            encoder = tiktoken.get_encoding(self.openai_model)
 
             # Calculate the number of tokens
             total_token_count = len(encoder.encode(combined_text))
