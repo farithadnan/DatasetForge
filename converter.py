97e0fdd98c56ece76e95409b2ff578aeb4fd52c6
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2023-11-03 11:48:38 +0800

    Refactor to_jsonl method to accept output directory as parameter

--- converter.py
+++ converter.py
@@ -2,21 +2,13 @@ import json
 import os
 from typing import List, Dict
 
-# Convert data to JSONL format
 class Converter:
+    def __init__(self, output_dir: str):
+        self.output_dir = output_dir  
 
     def to_jsonl(self, data: List[Dict[str, str]], filename: str) -> bool:
         try:
-            # Get set directories
-            current_dir = os.path.dirname(os.path.abspath(__file__))
-            output_dir = os.path.join(current_dir, "Output")
-
-            # Check if "output" folder exists, and create it if not
-            if not os.path.exists(output_dir):
-                os.makedirs(output_dir)
-
-            # Add "output/" before filename to save the file in the "output" folder
-            filename_with_path = os.path.join(output_dir, filename)
+            filename_with_path = os.path.join(self.output_dir, filename)
             
             # Convert data to JSONL format
             with open(filename_with_path, "w") as f:
